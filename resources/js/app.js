1af62f5f10aef7d3e5048c1ac468caf1a040ab08
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-26 00:02:59 +0300

    connect vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
-
+import { store } from './store/store'
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.component('home-component', require('./components/HomeComponent.vue').default);
 
 const app = new Vue({
+    store,
     el: '#app',
 });
